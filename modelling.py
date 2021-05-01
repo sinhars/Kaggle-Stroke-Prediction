7689545ff8bed601cb7f524eebc836c18c5cea3f
Harsh V Singh <harshv.singh@outlook.com>
2021-05-02 01:10:05 +0530

    Data exploration notebook v2

--- modelling.py
+++ modelling.py
@@ -1,31 +1,64 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder, RobustScaler
+from sklearn.impute import KNNImputer
+from sklearn.metrics import confusion_matrix, classification_report, f1_score
 
 class Modelling():
     #Class Constructor
     def __init__(self, vis):
-        self.np = __import__("numpy")
-        self.pd = __import__("pandas")
-        self.sklearn = __import__("sklearn")
-        self.metrics = self.sklearn.metrics
-        self.mod_select = self.sklearn.model_selection
         self.vis = vis
 
     def splitData(self, X, y, testSize, randomSeed, resetIndex=False):
-        splits = self.mod_select.train_test_split(X, y, test_size=testSize, stratify=y, random_state=randomSeed)
+        splits = train_test_split(X, y, test_size=testSize, stratify=y, random_state=randomSeed)
         if resetIndex == True:
             splits = [df.reset_index(drop=True) for df in splits]
         return (splits)
 
-    def preprocData(self, X, y, highCorrColumns, scalerModel, lowVarColumns):
-        X_no_corrs = X.drop(columns=highCorrColumns)
-        X_scaled = self.pd.DataFrame(scalerModel.transform(X_no_corrs))
-        X_scaled.columns = X_no_corrs.columns
-        X_preproc = X_scaled.drop(columns=lowVarColumns)
-        return (X_preproc, y)
+    def oneHotEncodeData(self, X, categoryColumns, encoderModels=None):
+        X_enc = X.copy(deep=True)
+        if encoderModels is None:
+            encoderModels = {}
+        for col in categoryColumns:
+            if col not in encoderModels.keys():
+                colEncoder = OneHotEncoder(handle_unknown="ignore")
+                colEncoder.fit(X_enc[[col]])
+                encoderModels[col] = colEncoder
+            else:
+                colEncoder = encoderModels[col]
+            encData = pd.DataFrame(colEncoder.transform(X[[col]]).toarray(), columns=np.array(colEncoder.categories_).flatten())
+            X_enc = pd.concat([X_enc, encData], axis=1)
+            X_enc.drop(columns=col, inplace=True)
+        return (X_enc, encoderModels)
+
+    def imputeData(self, X, imputerModel=None):
+        if imputerModel is None:
+            imputerModel = KNNImputer()
+            imputerModel.fit(X)
+        imputedData = imputerModel.transform(X)
+        X_imp = pd.DataFrame(imputedData, columns=X.columns)
+        return(X_imp, imputerModel)
+    
+    def scaleData(self, X, numericColumns, scalerModel=None):
+        if scalerModel is None:
+            scalerModel = RobustScaler()
+            scalerModel.fit(X[numericColumns])
+        scaledData = pd.DataFrame(scalerModel.transform(X[numericColumns]), columns=numericColumns)
+        X_scaled = X.drop(columns=numericColumns)
+        X_scaled = pd.concat([X_scaled, scaledData], axis=1)
+        return (X_scaled, scalerModel)
+
+    def preprocData(self, X, categoryColumns, numericColumns, encoderModels=None, imputerModel=None, scalerModel=None):
+        X_enc, encoderModels = self.oneHotEncodeData(X=X, categoryColumns=categoryColumns, encoderModels=encoderModels)
+        X_imp, imputerModel = self.imputeData(X=X_enc, imputerModel=imputerModel)
+        X_scaled, scalerModel = self.scaleData(X=X_imp, numericColumns=numericColumns, scalerModel=scalerModel)
+        return (X_scaled, encoderModels, imputerModel, scalerModel)
 
     #Method to plot counts/ histogram
     def getModelPerformance(self, trueVals, preds, figSize, plotTitle, targetNames):
-        confMatrix = self.metrics.confusion_matrix(trueVals, preds, normalize="true")
-        report = self.metrics.classification_report(trueVals, preds, target_names=targetNames)
-        print("F1-score = %.2f%% \n" % (self.metrics.f1_score(trueVals, preds) * 100))
+        confMatrix = confusion_matrix(trueVals, preds, normalize="true")
+        report = classification_report(trueVals, preds, target_names=targetNames)
+        print("F1-score = %.2f%% \n" % (f1_score(trueVals, preds) * 100))
         print(report)
         self.vis.plotConfusionMatrix(confMatrix, labels=targetNames, figSize=figSize, plotTitle=plotTitle)
--- visualizations.py
+++ visualizations.py
@@ -1,72 +1,67 @@
-
-#Plotter class for various data exploration techniques
-from matplotlib.pyplot import title
-
+import math
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns 
 
 class Visualizations():
     #Class Constructor
     def __init__(self, style="seaborn", fontSize=12):
-        self.math = __import__("math")
-        self.np = __import__("numpy")
-        self.mpl = __import__("matplotlib")
-        self.sns = __import__("seaborn")
-        self.plt = self.mpl.pyplot
-
         params = {
             'axes.titlesize': fontSize+2,
             'xtick.labelsize': fontSize,
             'ytick.labelsize': fontSize
             }
-        self.mpl.rcParams.update(params)
-        self.plt.style.use(style)
-        self.cmap = self.sns.diverging_palette(10, 250, s=90, l=33, as_cmap=True)
+        mpl.rcParams.update(params)
+        plt.style.use(style)
+        self.cmap = sns.diverging_palette(10, 250, s=90, l=33, as_cmap=True)
 
     #Method to plot counts/ histogram
     def plotCounts(self, xData, figSize, plotTitle):
-        self.plt.figure(figsize=figSize)
-        self.sns.countplot(x=xData)
-        self.plt.title(plotTitle)
-        self.plt.show()
+        plt.figure(figsize=figSize)
+        sns.countplot(x=xData)
+        plt.title(plotTitle)
+        plt.show()
 
     def plotHistograms(self, data, figSize, bins=50):
         data.hist(figsize=figSize, bins=bins)
-        self.plt.show()
+        plt.show()
 
     def plotBoxPlot(self, data, x, y, hue, figSize, plotTitle):
-        self.plt.figure(figsize=figSize)
-        self.sns.boxplot(data=data, x=x, y=y, hue=hue)
-        self.plt.title(plotTitle)
-        self.plt.show()
+        plt.figure(figsize=figSize)
+        sns.boxplot(data=data, x=x, y=y, hue=hue)
+        plt.title(plotTitle)
+        plt.show()
 
     def plotMultipleBoxPlots(self, data, figSize, plotTitle):
-        self.plt.figure(figsize=figSize)
-        ax = self.sns.boxplot(data=data, orient="h")
+        plt.figure(figsize=figSize)
+        ax = sns.boxplot(data=data, orient="h")
         ax.set_title(plotTitle)
         ax.set(xscale="log")
-        self.plt.show()
+        plt.show()
 
     def plotMultipleCountPlots(self, data, columns, figSize, plotTitle):
-        ncols = self.math.floor(self.math.sqrt(len(columns)))
-        nrows = self.math.ceil(len(columns) / ncols)
-        fig = self.plt.figure(figsize=figSize, tight_layout=True)
+        ncols = math.floor(math.sqrt(len(columns)))
+        nrows = math.ceil(len(columns) / ncols)
+        fig = plt.figure(figsize=figSize, tight_layout=True)
         fig.suptitle(plotTitle)
         for i, col in enumerate(columns):
             fig.add_subplot(nrows, ncols, i + 1)
-            self.sns.countplot(x=col, data=data)
-            self.plt.xlabel("")
-            self.plt.title(col)
-        self.plt.show()
+            sns.countplot(x=col, data=data)
+            plt.xlabel("")
+            plt.title(col)
+        plt.show()
 
     def plotCorrelationMatrix(self, data, figSize, plotTitle):
         corrMatrix = data.corr()
-        maskMatrix = self.np.triu(self.np.ones_like(corrMatrix, dtype=bool))
-        self.plt.figure(figsize=figSize)
-        self.sns.heatmap(corrMatrix, mask=maskMatrix, cmap=self.cmap, annot=False)
-        self.plt.title(plotTitle)
-        self.plt.show()
+        maskMatrix = np.triu(np.ones_like(corrMatrix, dtype=bool))
+        plt.figure(figsize=figSize)
+        sns.heatmap(corrMatrix, mask=maskMatrix, cmap=self.cmap, annot=False)
+        plt.title(plotTitle)
+        plt.show()
     
     def plotConfusionMatrix(self, data, labels, figSize, plotTitle):
-        self.plt.figure(figsize=figSize)
-        self.sns.heatmap(data, cmap=self.cmap, xticklabels=labels, yticklabels=labels, annot=True, fmt=".1%")
-        self.plt.title(plotTitle)
-        self.plt.show()
+        plt.figure(figsize=figSize)
+        sns.heatmap(data, cmap=self.cmap, xticklabels=labels, yticklabels=labels, annot=True, fmt=".1%")
+        plt.title(plotTitle)
+        plt.show()
